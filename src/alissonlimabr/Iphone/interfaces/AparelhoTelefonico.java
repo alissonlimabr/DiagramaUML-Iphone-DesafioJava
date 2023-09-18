c316479f4e175679b4c446c1adbdd1d2e9ebec1c
Alisson Lima <alissonlimabr.github@gmail.com>
2023-09-17 23:14:35 -0500

    feat: interfaces do projeto

---
+++ src/alissonlimabr/Iphone/interfaces/AparelhoTelefonico.java
@@ -0,0 +1,7 @@
+package alissonlimabr.Iphone.interfaces;
+
+public interface AparelhoTelefonico {
+    void ligar(String numero);
+    void atender();
+    void iniciarCorreioVoz();
+}
---
+++ src/alissonlimabr/Iphone/interfaces/NavegadorInternet.java
@@ -0,0 +1,7 @@
+package alissonlimabr.Iphone.interfaces;
+
+public interface NavegadorInternet {
+    void exibirPagina(String url);
+    void adicionarNovaAba();
+    void atualizarPagina();
+}
